004b90a47bed091c0c29e0e895215f4fd5ed5358
Daniel T. Santos <danieltex@gmail.com>
2019-08-25 20:43:56 -0300

    Added Order add/remove logic

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,10 +2,12 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom');
+const ShortID = require("shortid")
 
 class Burger extends React.Component {
     constructor(props) {
         super(props);
+        // this.onClickAdd.bind(this);
     }
 
     renderIngredients() {
@@ -26,14 +28,35 @@ class Burger extends React.Component {
                 <ul>
                     {  this.renderIngredients() }
                 </ul>
-                <button onClick={ this.onClickAdd }>Adicionar</button>
+                <button onClick={ () => this.onClickAdd() }>Adicionar</button>
             </div>
         );
     }
 }
 
 class OrderItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
+    renderIngredients() {
+        const entries = Object.entries(this.props.burger.ingredients);
+        return entries.map(ing =>
+            <li>{ing[1]}: {ing[0]}</li>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="burger-name">{this.props.burger.name}</div>
+                <ul>
+                    {  this.renderIngredients() }
+                </ul>
+                <button onClick={ () => this.props.remove() }>Remover</button>
+            </div>
+        );
+    }
 }
 
 class App extends React.Component {
@@ -47,10 +70,10 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        this.makeRequest()
+        this.requestBurgerList()
     }
 
-    makeRequest() {
+    requestBurgerList() {
         fetch("/burgers")
             .then(
                 result => result.json()
@@ -58,22 +81,54 @@ class App extends React.Component {
             .then( resultJson => {
                 this.setState({
                     burgers: resultJson,
-                    isLoaded: true
+                    isLoaded: true,
+                    orderItems: {}
                 });
             })
             .catch(error => this.setState({error}));
     }
 
+    addOrderItem(burger) {
+        const prevOrderItems = this.state.orderItems;
+        const orderItems = {...prevOrderItems};
+        orderItems[ShortID.generate()] = burger;
+        this.setState(prevState => ({
+            ...prevState,
+            orderItems:  orderItems
+        }))
+    }
+
+    removeOrderItem(key) {
+        const prevOrderItems = this.state.orderItems;
+        const orderItems = {...prevOrderItems};
+        delete  orderItems[key];
+        this.setState(prevState => ({
+            ...prevState,
+            orderItems:  orderItems
+        }))
+    }
+
     render() {
-        const {error, isLoaded, burgers} = this.state;
+        const {error, isLoaded, burgers, orderItems} = this.state;
         if (error) {
             return <div>error.message</div>;
         } else if (!isLoaded) {
             return <div>Carregando...</div>
         } else {
-            return burgers.map(burger =>
-                <Burger key={burger.name} burger={burger} />
+            const burgerComponents = burgers.map(burger =>
+                <Burger key={burger.name} burger={burger} add={(burger) => this.addOrderItem(burger)}/>
             );
+            if (Object.keys(orderItems).length !== 0) {
+                burgerComponents.push(
+                    <div>
+                        <h2>Pedido</h2>
+                        { Object.entries(orderItems).map(entry =>
+                            <OrderItem key={entry[0]} burger={entry[1]}
+                            remove={() => this.removeOrderItem(entry[0])}/>) }
+                    </div>
+                )
+            }
+            return burgerComponents
         }
     }
 
